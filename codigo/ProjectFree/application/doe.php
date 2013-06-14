35784f0bf6df33bebd530491e0aa51f75ca29e98
BrunoRB <brunorb8@hotmail.com>
2013-06-14 01:12:50 -0300

    adicionada páginas e design

---
+++ codigo/ProjectFree/application/doe.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../templates/header.php';
+
+?>
+
+<div class="container">
+	
+</div>
+
+<?php
+
+include_once '../templates/footer.php';
--- codigo/ProjectFree/templates/footer.php
+++ codigo/ProjectFree/templates/footer.php
@@ -1,9 +1,11 @@
 
 
 	
-		<footer>
-			<strong>&copy;Copyright</strong> <br />
-			<small>Bruno Roberto, Roberto, Fabricio, Maikon, Ebara</small>
+		<footer class="footer navbar-fixed-bottom">
+			<div class="pagination-centered">
+				<strong>&copy;Copyright</strong> <br />
+				<em>Bruno Roberto, Roberto, Fabricio, Maikon, Ebara</em>
+			</div>
 		</footer>
 	</body>
 </html>
