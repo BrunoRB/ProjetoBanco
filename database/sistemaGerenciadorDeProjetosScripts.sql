6d60c90bac2c28fc172128d67749de8000b6ace6
BrunoRB <brunorb8@hotmail.com>
2013-07-01 20:07:17 -0300

    adicionadas tabelas faleconsco e doação

--- database/sistemaGerenciadorDeProjetosScripts.sql
+++ database/sistemaGerenciadorDeProjetosScripts.sql
@@ -23,6 +23,28 @@ CREATE TABLE usuario
   CONSTRAINT check_senha_length CHECK (senha ~ '\w{5,}')
 );
 
+CREATE TABLE fale_conosco
+(
+	id_forma_de_contato SERIAL NOT NULL,
+	data_e_hora TIMESTAMP NOT NULL DEFAULT NOW(),
+	assunto VARCHAR(100) NOT NULL,
+	mensagem TEXT NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	fk_usuario INTEGER NOT NULL,
+	CONSTRAINT pk_fale_conosco PRIMARY KEY (id_forma_de_contato),
+	CONSTRAINT fk_usuario FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
+);
+
+CREATE TABLE doacao
+(
+	id_doacao SERIAL NOT NULL,
+	quantia NUMERIC(11, 2) NOT NULL,
+	data_e_hora TIMESTAMP NOT NULL DEFAULT NOW(),
+	fk_doador INTEGER NOT NULL,
+	CONSTRAINT pk_doacao PRIMARY KEY (id_doacao),
+	CONSTRAINT fk_usuario FOREIGN KEY (fk_doador) REFERENCES usuario (id_usuario)
+);
+
 CREATE TABLE membro
 (
   id_membro SERIAL NOT NULL,
@@ -30,22 +52,22 @@ CREATE TABLE membro
   fk_usuario INTEGER NOT NULL,
   CONSTRAINT pk_membro PRIMARY KEY (id_membro),
   CONSTRAINT fk_membro FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
-  CONSTRAINT check_data_de_nascimento CHECK (data_de_nascimento::text ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
+  CONSTRAINT check_data_de_nascimento CHECK (data_de_nascimento::TEXT ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
 );
 
 CREATE TABLE projeto
 (
    id_projeto SERIAL NOT NULL, 
    nome CHARACTER VARYING(100) NOT NULL, 
-   orcamento NUMERIC(20,4), 
+   orcamento NUMERIC(11,2), 
    data_de_cadastro DATE NOT NULL,
-   descricao text, 
+   descricao TEXT, 
    fk_gerente INTEGER NOT NULL, 
    CONSTRAINT pk_projeto PRIMARY KEY (id_projeto), 
    CONSTRAINT fk_gerente_de_projeto FOREIGN KEY (fk_gerente) REFERENCES membro (id_membro),
    CONSTRAINT check_nome CHECK (nome ~ '\w{5,}'),
-   CONSTRAINT check_orcamento CHECK (orcamento::text ~* '^\d'),
-   CONSTRAINT check_data_de_cadastro CHECK (data_de_cadastro::text ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
+   CONSTRAINT check_orcamento CHECK (orcamento::TEXT ~* '^\d'),
+   CONSTRAINT check_data_de_cadastro CHECK (data_de_cadastro::TEXT ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
 );
 
 
@@ -53,13 +75,13 @@ CREATE TABLE despesa
 (
   id_despesa SERIAL NOT NULL,
   nome CHARACTER VARYING(100) NOT NULL,
-  valor NUMERIC(20,4) NOT NULL,
-  descricao text,
+  valor NUMERIC(11,2) NOT NULL,
+  descricao TEXT,
   fk_projeto INTEGER NOT NULL,
   CONSTRAINT pk_despesa PRIMARY KEY (id_despesa),
   CONSTRAINT fk_projeto_desp FOREIGN KEY (fk_projeto) REFERENCES projeto (id_projeto),
   CONSTRAINT check_nome CHECK (nome ~* '\w{5,}'),
-  CONSTRAINT check_valor CHECK (valor::text ~ '^\d')
+  CONSTRAINT check_valor CHECK (valor::TEXT ~ '^\d')
 );
 
 
@@ -67,7 +89,7 @@ CREATE TABLE recurso
 (
   id_recurso SERIAL NOT NULL,
   nome CHARACTER VARYING(100) NOT NULL,
-  descricao text,
+  descricao TEXT,
   fk_projeto INTEGER NOT NULL,
   fk_despesa INTEGER,
   CONSTRAINT pk_recurso PRIMARY KEY (id_recurso),
@@ -86,8 +108,8 @@ CREATE TABLE cronograma
    fk_projeto INTEGER NOT NULL, 
    CONSTRAINT pk_cronograma PRIMARY KEY (id_cronograma), 
    CONSTRAINT fk_projeto_crono FOREIGN KEY (fk_projeto) REFERENCES projeto (id_projeto),
-   CONSTRAINT check_data_inicio_projeto CHECK (data_inicio_projeto::text ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$'),
-   CONSTRAINT check_data_limite_projeto CHECK (data_limite_projeto::text ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
+   CONSTRAINT check_data_inicio_projeto CHECK (data_inicio_projeto::TEXT ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$'),
+   CONSTRAINT check_data_limite_projeto CHECK (data_limite_projeto::TEXT ~* '^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$')
 );
 
 CREATE TABLE atividade
@@ -97,7 +119,7 @@ CREATE TABLE atividade
   data_limite_atividade TIMESTAMP NOT NULL, --TODO
   data_fim_atividade TIMESTAMP, --TODO
   nome_atividade CHARACTER VARYING(100) NOT NULL,
-  descricao_atividade text,
+  descricao_atividade TEXT,
   fk_projeto INTEGER NOT NULL,
   fk_cronograma INTEGER NOT NULL,
   CONSTRAINT pk_atividade PRIMARY KEY (id_atividade),
@@ -111,7 +133,7 @@ CREATE TABLE atividade
 CREATE TABLE comentario
 (
   id_comentario SERIAL NOT NULL,
-  descricao text,
+  descricao TEXT,
   data_horario_envio TIMESTAMP, --TODO
   fk_atividade INTEGER NOT NULL,
   CONSTRAINT pk_comentario PRIMARY KEY (id_comentario),
@@ -131,7 +153,7 @@ CREATE TABLE log_de_erro
 (
   id_log_de_erro SERIAL NOT NULL,
   tabela CHARACTER VARYING(100) NOT NULL,
-  descricao_erro text,
+  descricao_erro TEXT,
   CONSTRAINT pk_log_de_erro PRIMARY KEY (id_log_de_erro)
 );
 
@@ -171,7 +193,7 @@ CREATE TABLE mensagem
 (
    id_mensagem SERIAL NOT NULL, 
    assunto CHARACTER VARYING(100), 
-   texto text, 
+   TEXTo TEXT, 
    CONSTRAINT pk_mensagem PRIMARY KEY (id_mensagem)
 );
 
