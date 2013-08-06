0d70ddf145d4ef93743f3f9748a91495a5aff86a
BrunoRB <brunorb8@hotmail.com>
2013-08-06 20:33:15 -0300

    instalador, scripts e inserção e tabelas atividade&projeto

--- database/funcoes/atividade.sql
+++ database/funcoes/atividade.sql
@@ -1,47 +1,32 @@
 
 --INSERTS;
 
-CREATE OR REPLACE FUNCTION atividadeInsert (id INTEGER, inicio TIMESTAMP, limite TIMESTAMP, nome VARCHAR(100), descricao TEXT, id_projeto INTEGER) RETURNS INTEGER AS $$
-	DECLARE
-		id_atividade INTEGER;
-	BEGIN
-		IF (inicio > limite) THEN
-			RAISE NOTICE 'Data de limite da atividade deve ser maior que data de início !';
-			RETURN 0;
-		ELSE
-			INSERT INTO atividade (id_atividade, inicio_atividade, limite_atividade, nome_atividade, descricao_atividade, fk_projeto) 
-				VALUES (id, inicio, limite, nome, descricao, id_projeto) RETURNING id_atividade INTO id_atividade;
-			RETURN id_atividade;
-		END IF;
-	END;
-$$ LANGUAGE PLPGSQL;
-
 CREATE OR REPLACE FUNCTION atividadeInsert (inicio TIMESTAMP, limite TIMESTAMP, nome VARCHAR(100), descricao TEXT, id_projeto INTEGER) RETURNS INTEGER AS $$
 	DECLARE
-		id_atividade INTEGER;
+		id_gerada  INTEGER;
 	BEGIN
 		IF (inicio > limite) THEN
 			RAISE NOTICE 'Data de limite da atividade deve ser maior que data de início !';
 			RETURN 0;
 		ELSE
 			INSERT INTO atividade (inicio_atividade, limite_atividade, nome_atividade, descricao_atividade, fk_projeto) 
-				VALUES (inicio, limite, nome, descricao, id_projeto) RETURNING id_atividade INTO id_atividade;
-			RETURN id_atividade;
+				VALUES (inicio, limite, nome, descricao, id_projeto) RETURNING id_atividade INTO id_gerada ;
+			RETURN id_gerada;
 		END IF;
 	END;
 $$ LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE FUNCTION atividadeInsert (inicio TIMESTAMP, limite TIMESTAMP, nome VARCHAR(100), id_projeto INTEGER) RETURNS INTEGER AS $$
 	DECLARE
-		id_atividade INTEGER;
+		id_gerada  INTEGER;
 	BEGIN
 		IF (inicio > limite) THEN
 			RAISE NOTICE 'Data de limite da atividade deve ser maior que data de início !';
 			RETURN 0;
 		ELSE
 			INSERT INTO atividade (inicio_atividade, limite_atividade, nome_atividade, fk_projeto) 
-				VALUES (inicio, limite, nome, id_projeto) RETURNING id_atividade INTO id_atividade;
-			RETURN id_atividade;
+				VALUES (inicio, limite, nome, id_projeto) RETURNING id_atividade  INTO id_gerada ;
+			RETURN id_gerada;
 		END IF;
 	END;
 $$ LANGUAGE PLPGSQL;
@@ -50,7 +35,7 @@ $$ LANGUAGE PLPGSQL;
 
 --UPDATES;
 
-CREATE OR REPLACE FUNCTION atividadeUpdate (inicio TIMESTAMP, limite TIMESTAMP, fim TIMESTAMP, nome VARCHAR(100), descricao TEXT, finished BOOLEAN, id_projeto INTEGER) 
+CREATE OR REPLACE FUNCTION atividadeUpdate (id INTEGER, inicio TIMESTAMP, limite TIMESTAMP, fim TIMESTAMP, nome VARCHAR(100), descricao TEXT, finished BOOLEAN) 
 	RETURNS INTEGER AS $$
 	BEGIN
 		IF (inicio > limite) THEN
@@ -60,8 +45,12 @@ CREATE OR REPLACE FUNCTION atividadeUpdate (inicio TIMESTAMP, limite TIMESTAMP, 
 			RAISE NOTICE 'Data de fim da atividade deve ser maior que data de início !';
 			RETURN 0;
 		ELSE
-			UPDATE atividade SET inicio_atividade=inicio, limite_atividade=limite, fim_atividade=fim, nome_atividade=nome, descricao_atividade=descricao, 					finalizada=finished WHERE fk_projeto = id_projeto;
-			RETURN 1;
+			UPDATE atividade SET inicio_atividade=inicio, limite_atividade=limite, fim_atividade=fim, nome_atividade=nome, descricao_atividade=descricao, 					finalizada=finished WHERE id_atividade = id;
+			IF (FOUND) THEN
+				RETURN 1;
+			ELSE
+				RETURN 0;
+			END IF;
 		END IF;
 	END;
 $$ LANGUAGE PLPGSQL;
@@ -73,15 +62,13 @@ $$ LANGUAGE PLPGSQL;
 CREATE OR REPLACE FUNCTION atividadeDelete(id_projeto INTEGER) RETURNS INTEGER AS $$
 BEGIN
 	DELETE FROM atividade WHERE fk_projeto = id_projeto;
+	IF (FOUND) THEN
+		RETURN 1;
+	ELSE
+		RETURN 0;
+	END IF;
 END;
 $$ LANGUAGE PLPGSQL;
 
 --END DELETES;
 
---SELECTS;
-
-CREATE OR REPLACE FUNCTION atividadeSelect(id_projeto INTEGER) RETURNS SETOF atividade AS $$
-BEGIN
-	RETURN QUERY EXECUTE 'SELECT dinicio_atividade, limite_atividade, fim_atividade, nome_atividade, descricao_atividade, finalizada FROM atividade WHERE fk_projeto = 			id_projeto';
-END;
-$$ LANGUAGE PLPGSQL;
--- database/funcoes/projeto.sql
+++ database/funcoes/projeto.sql
@@ -1,90 +1,65 @@
 
 --INSERTS;
 
-CREATE OR REPLACE FUNCTION projetoInsert (id INTEGER, nome VARCHAR(100), orcamento NUMERIC(10, 2), descricao TEXT, fk_gerente INTEGER)
-RETURNS INTEGER AS $$
-DECLARE
-	id_projeto INTEGER;
-BEGIN
-	INSERT INTO projeto (id_projeto, nome, orcamento, descricao, fk_gerente) 
-		VALUES (id, nome, orcamento, descricao, fk_gerente) RETURNING id_projeto INTO id_projeto;
-	RETURN id_projeto;
-END;
+CREATE OR REPLACE FUNCTION projetoCadastrar (nome VARCHAR(100), orcamento NUMERIC(10, 2), descricao TEXT, id_gerente INTEGER) RETURNS INTEGER AS $$
+	DECLARE
+		id_gerada INTEGER;
+	BEGIN
+		INSERT INTO projeto (nome, orcamento, descricao, fk_gerente) VALUES 
+			(nome, orcamento, descricao, id_gerente) RETURNING id_projeto INTO id_gerada;
+		RETURN id_gerada;
+	END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION projetoInsert (nome VARCHAR(100), orcamento NUMERIC(10, 2), descricao TEXT, fk_gerente INTEGER)
-RETURNS INTEGER AS $$
-DECLARE
-	id_projeto INTEGER;
-BEGIN
-	INSERT INTO projeto (nome, orcamento, descricao, fk_gerente) VALUES 
-		(nome, orcamento, descricao, fk_gerente) RETURNING id_projeto INTO id_projeto;
-	RETURN id_projeto;
-END;
+CREATE OR REPLACE FUNCTION projetoCadastrar (nome VARCHAR(100), descricao TEXT, id_gerente INTEGER) RETURNS INTEGER AS $$
+	DECLARE
+		id_gerada INTEGER;
+	BEGIN
+		INSERT INTO projeto (nome, descricao, fk_gerente) VALUES (nome, descricao, id_gerente) RETURNING id_projeto INTO id_gerada;
+		RETURN id_gerada;
+	END;
 $$ LANGUAGE PLPGSQL;
 
 
-CREATE OR REPLACE FUNCTION projetoInsert (nome VARCHAR(100), descricao TEXT, fk_gerente INTEGER)
-RETURNS INTEGER AS $$
-DECLARE
-	id_projeto INTEGER;
-BEGIN
-	INSERT INTO projeto (nome, descricao, fk_gerente) VALUES (id, nome, descricao, fk_gerente) RETURNING id_projeto INTO id_projeto;
-	RETURN id_projeto;
-END;
-$$ LANGUAGE PLPGSQL;
-
-
-CREATE OR REPLACE FUNCTION projetoInsert (nome VARCHAR(100), orcamento NUMERIC(10, 2), fk_gerente INTEGER)
-RETURNS INTEGER AS $$
-DECLARE
-	id_projeto INTEGER;
-BEGIN
-	INSERT INTO projeto (nome, orcamento, fk_gerente) VALUES (nome, orcamento, fk_gerente) RETURNING id_projeto INTO id_projeto;
-	RETURN id_projeto;
-END;
+CREATE OR REPLACE FUNCTION projetoCadastrar (nome VARCHAR(100), orcamento NUMERIC(10, 2), id_gerente INTEGER) RETURNS INTEGER AS $$
+	DECLARE
+		id_gerada INTEGER;
+	BEGIN
+		INSERT INTO projeto (nome, orcamento, fk_gerente) VALUES (nome, orcamento, id_gerente) RETURNING id_projeto INTO id_gerada;
+		RETURN id_gerada;
+	END;
 $$ LANGUAGE PLPGSQL;
 
 --END INSERTS;
 
 --UPDATES;
 
-CREATE OR REPLACE FUNCTION projetoUpdate (id INTEGER, nome VARCHAR(100), orcamento NUMERIC(10, 2), descricao TEXT, fk_gerente INTEGER)
-RETURNS INTEGER AS $$
-BEGIN
-	UPDATE projeto SET projeto.nome=nome, projeto.orcamento = orcamento, projeto.descricao = descricao, projeto.fk_gerente = fk_gerente WHERE id_projeto = id;
-	RETURN 1;
-END;
+CREATE OR REPLACE FUNCTION projetoAtualizar (id INTEGER, nome VARCHAR(100), orcamento NUMERIC(10, 2), descricao TEXT, id_gerente INTEGER) RETURNS INTEGER AS $$
+	BEGIN
+		UPDATE projeto SET projeto.nome=nome, projeto.orcamento = orcamento, projeto.descricao = descricao, projeto.fk_gerente = id_gerente WHERE id_projeto = id;
+		IF (FOUND) THEN
+			RETURN 1;
+		ELSE
+			RETURN 0;
+		END IF;
+	END;
 $$ LANGUAGE PLPGSQL;
 
 --END UPDATES;
 
 --DELETES;
 
-CREATE OR REPLACE FUNCTION projetoDelete (id INTEGER)
-RETURNS INTEGER AS $$
-BEGIN
-	DELETE FROM projeto WHERE projeto.id = id;
-	RETURN 1;
-END;
+CREATE OR REPLACE FUNCTION projetoExcluir (id INTEGER)	RETURNS INTEGER AS $$
+	BEGIN
+		DELETE FROM projeto WHERE id_projeto = id;
+		IF (FOUND) THEN
+			RETURN 1;
+		ELSE
+			RETURN 0;
+		END IF;
+	END;
 $$ LANGUAGE PLPGSQL;
 
 --END DELETES;
 
---SELECTS;
 
-CREATE OR REPLACE FUNCTION projetoSelectByGerente (fk_gerente INTEGER)
-RETURNS SETOF projeto AS $$
-BEGIN
-	RETURN QUERY EXECUTE 'SELECT nome, orcamento, data_de_cadastro, descricao FROM projeto INNER JOIN membro ON projeto.fk_gerente = membro.id_membro';
-END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE FUNCTION projetoSelectById (id INTEGER)
-RETURNS SETOF projeto AS $$
-BEGIN
-	RETURN QUERY EXECUTE 'SELECT nome, orcamento, data_de_cadastro, descricao FROM projeto WHERE projeto.id_projeto = id';
-END;
-$$ LANGUAGE PLPGSQL;
-
---END SELECTS;
