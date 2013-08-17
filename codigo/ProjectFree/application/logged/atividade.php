e16f9a6072cef98b9af6369aab7b6e97aaded878
BrunoRB <brunorb8@hotmail.com>
2013-08-17 16:57:18 -0300

    tela de atividade

--- codigo/ProjectFree/application/logged/atividade.php
+++ codigo/ProjectFree/application/logged/atividade.php
@@ -10,18 +10,124 @@ class Atividade extends Main {
 
 	public function loadBody() {
 		if (isset($_GET['novo']) && $_GET['novo'] == true) {
-			$this->cadastrarAtividade();
+			$this->salvarAtividade();
 		}
 		else {
-
+			if (true) { //TODO Check gerente do projeto
+				$this->atividadesGerente();
+			}
+			else {
+				$this->atividadesMembro();
+			}
 		}
 	}
 
-	private function atividades() {
+	private function atividadesGerente() {
+		?>
+		<div class="row-fluid">
+			<div class="span7 collapse-group accordion-group" id="emAndamento">
+				<table class="table table-hover-mod">
+				 	<caption>
+				 		<a data-toggle="collapse" data-target="#emAndamento">Atividades em andamento</a>
+				 	</caption>
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>Iníciada em</th>
+							<th>Data limite</th>
+							<th>Predecessora</th>
+							<th>Fase</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Definição do escopo</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td></td>
+							<td>Elaboração</td>
+						</tr>
+						<tr>
+							<td>Protótipos de tela</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td></td>
+							<td>Construção</td>
+						</tr>
+						<tr>
+							<td>Documento de visão e escopo</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td>Definição do escopo</td>
+							<td>Levantamento de requisitos</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="span5">
+				<form action="<?php echo SAVE_FILE;?>" method="post">
+					<a class="btn btn-primary btn-large" href="atividade.php?novo=true">Cadastrar nova atividade</a>
+				</form>
+			</div>
+		</div>
 
+		<br> <!-- REMOVER BRBR! -->
+
+		<div class="row-fluid">
+			<div class="span7 collapse-group accordion-group" id="completas">
+				<table class="table table-hover-mod">
+				 	<caption>
+				 		<a data-toggle="collapse" data-target="#completas">Atividades completas</a>
+				 	</caption>
+					<thead>
+						<tr>
+							<th>Terminada em</th>
+							<th>Nome</th>
+							<th>Iníciada em</th>
+							<th>Data limite</th>
+							<th>Predecessora</th>
+							<th>Fase</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>xx/xx/xxxx</td>
+							<td>Definição do escopo</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td></td>
+							<td>Elaboração</td>
+						</tr>
+						<tr>
+							<td>xx/xx/xxxx</td>
+							<td>Protótipos de tela</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td></td>
+							<td>Construção</td>
+						</tr>
+						<tr>
+							<td>xx/xx/xxxx</td>
+							<td>Documento de visão e escopo</td>
+							<td>xx/xx/xxxx</td>
+							<td>xx/xx/xxxx</td>
+							<td>Definição do escopo</td>
+							<td>Levantamento de requisitos</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<?php
 	}
 
-	private function cadastrarAtividade() {
+	private function atividadesMembro() {
+		?>
+
+		<?php
+	}
+
+	private function salvarAtividade() {
 		?>
 		<div class="container">
 			<form action="<?php echo SAVE_FILE;?>" method="post">
