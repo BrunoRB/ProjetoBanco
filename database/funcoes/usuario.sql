ba167aef2e3bf104c7d94ba42c78596755433307
Maikon Marin <maikon_marin@hotmail.com>
2013-08-17 16:18:01 -0300

    atualização usuario.sql função logar

--- database/funcoes/usuario.sql
+++ database/funcoes/usuario.sql
@@ -5,6 +5,7 @@ DECLARE
 BEGIN 
 	INSERT INTO usuario (id_usuario, nome, login, senha, inativo, data_inatividade)
 		VALUES (id, nome, login, md5(senha), inativo, inatividade) RETURNING id_usuario INTO id_gerada;
+		RAISE NOTICE 'Usuário cadastrado com sucesso!';
 		RETURN id_gerada;
 EXCEPTION 
 	WHEN CHECK_VIOLATION THEN
@@ -21,6 +22,7 @@ DECLARE
 BEGIN 
 	INSERT INTO usuario (nome, login, senha, inativo, data_inatividade)
 		VALUES (nome, login, md5(senha), inativo, inatividade) RETURNING id_usuario INTO id_gerada;
+		RAISE NOTICE 'Usuário cadastrado com sucesso!';
 		RETURN id_gerada;
 EXCEPTION 
 	WHEN CHECK_VIOLATION THEN
@@ -38,6 +40,7 @@ DECLARE
 BEGIN 
 	INSERT INTO usuario (nome, login, senha)
 		VALUES (nome, login, md5(senha)) RETURNING id_usuario INTO id_gerada;
+		RAISE NOTICE 'Usuário cadastrado com sucesso!';
 		RETURN id_gerada;
 EXCEPTION 
 	WHEN CHECK_VIOLATION THEN
@@ -48,19 +51,43 @@ $$ LANGUAGE PLPGSQL;
 
 
 
-
 CREATE OR REPLACE FUNCTION usuarioExcluir (id INTEGER)
 RETURNS INTEGER AS $$
 BEGIN 
 	UPDATE usuario SET inativo = TRUE, data_inatividade = CURRENT_DATE WHERE id_usuario = (id);
 	IF (FOUND) THEN
+		RAISE NOTICE 'Usuário inativado com sucesso!';
 		RETURN 1;
 	ELSE
+		RAISE NOTICE 'Usuário não inativado!';
 		RETURN 0;
 	END IF;
 EXCEPTION 
 	WHEN CHECK_VIOLATION THEN
 		RAISE NOTICE '[Erro] Dados inválidos inseridos !';
+		RETURN 0;
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+
+CREATE OR REPLACE FUNCTION logar (logon VARCHAR, password VARCHAR)
+RETURNS INTEGER AS $$
+DECLARE
+	id_gerada INT;
+	name VARCHAR;
+BEGIN 
+	SELECT INTO id_gerada, name id_usuario, nome FROM usuario WHERE login = logon AND senha = md5(password);
+	IF (FOUND) THEN
+		RAISE NOTICE 'Olá, %', name;
+		RETURN id_gerada;
+	ELSE
+		RAISE NOTICE '[Erro] Login ou senha inválidos!';
+		RETURN 0;
+	END IF;
+EXCEPTION 
+	WHEN CHECK_VIOLATION THEN
+		RAISE NOTICE '[Erro] Dados inválidos inseridos!';
 		RETURN 0;
 END;
 $$ LANGUAGE PLPGSQL;
