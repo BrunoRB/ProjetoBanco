7fe9dbd5ae0cd86c263ecb0d4a010bdddac3a5c8
Maikon Marin <maikon_marin@hotmail.com>
2013-08-20 02:08:12 -0300

    alteração usuario.sql, exclusão das funções sem utilidade e alteração da ordem do campo email

--- database/funcoes/usuario.sql
+++ database/funcoes/usuario.sql
@@ -1,64 +1,12 @@
 --INSERTS;
 
---todos;
-CREATE OR REPLACE FUNCTION usuarioCadastrar (id INTEGER, nome VARCHAR, senha VARCHAR, email VARCHAR, imagem VARCHAR,inativo BOOLEAN, inatividade DATE)
+CREATE OR REPLACE FUNCTION usuarioCadastrar (nome VARCHAR, email VARCHAR, senha VARCHAR)
 RETURNS INTEGER AS $$
 DECLARE
 	id_gerada INT;
 BEGIN 
-	INSERT INTO usuario (id_usuario, nome, senha, email, imagem, inativo, data_inatividade)
-		VALUES (id, nome, md5(senha), email, imagem, inativo, inatividade) RETURNING id_usuario INTO id_gerada;
-		RAISE NOTICE 'Usuário cadastrado com sucesso!';
-		RETURN id_gerada;
-EXCEPTION 
-	WHEN CHECK_VIOLATION THEN
-		RAISE NOTICE '[Erro] Dados inválidos inseridos!';
-		RETURN 0;
-END;
-$$ LANGUAGE PLPGSQL;
-
-
-CREATE OR REPLACE FUNCTION usuarioCadastrar (nome VARCHAR, senha VARCHAR, email VARCHAR, imagem VARCHAR, inativo BOOLEAN, inatividade DATE)
-RETURNS INTEGER AS $$
-DECLARE
-	id_gerada INT;
-BEGIN 
-	INSERT INTO usuario (nome, senha, email, imagem, inativo, data_inatividade)
-		VALUES (nome, md5(senha), email, imagem, inativo, inatividade) RETURNING id_usuario INTO id_gerada;
-		RAISE NOTICE 'Usuário cadastrado com sucesso!';
-		RETURN id_gerada;
-EXCEPTION 
-	WHEN CHECK_VIOLATION THEN
-		RAISE NOTICE '[Erro] Dados inválidos inseridos!';
-		RETURN 0;
-END;
-$$ LANGUAGE PLPGSQL;
-
-
-CREATE OR REPLACE FUNCTION usuarioCadastrar (nome VARCHAR, senha VARCHAR, email VARCHAR, imagem VARCHAR)
-RETURNS INTEGER AS $$
-DECLARE
-	id_gerada INT;
-BEGIN 
-	INSERT INTO usuario (nome, senha, email, imagem)
-		VALUES (nome, md5(senha), email, imagem) RETURNING id_usuario INTO id_gerada;
-		RAISE NOTICE 'Usuário cadastrado com sucesso!';
-		RETURN id_gerada;
-EXCEPTION 
-	WHEN CHECK_VIOLATION THEN
-		RAISE NOTICE '[Erro] Dados inválidos inseridos!';
-		RETURN 0;
-END;
-$$ LANGUAGE PLPGSQL;
-
-
-CREATE OR REPLACE FUNCTION usuarioCadastrar (nome VARCHAR, senha VARCHAR, email VARCHAR)
-RETURNS INTEGER AS $$
-DECLARE
-	id_gerada INT;
-BEGIN 
-	INSERT INTO usuario (nome, senha, email)
-		VALUES (nome, md5(senha), email) RETURNING id_usuario INTO id_gerada;
+	INSERT INTO usuario (nome, email, senha)
+		VALUES (nome, email, md5(senha)) RETURNING id_usuario INTO id_gerada;
 		RAISE NOTICE 'Usuário cadastrado com sucesso!';
 		RETURN id_gerada;
 EXCEPTION 
