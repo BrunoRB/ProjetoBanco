074be0e3a04a53d5d4fd7ec788f80da889571b77
BrunoRB <brunorb8@hotmail.com>
2013-08-29 11:14:38 -0300

    roles finished, callFunctions created (leia o arquivo para informações)

---
+++ database/callFunctions.sql
@@ -0,0 +1,17 @@
+
+-- Chame por "\i funcoes/callFunctions.sql" no psql. É necessário estar na pasta "ProjetoBanco"
+
+SET CLIENT_ENCODING='latin1';
+
+\i database/funcoes/usuario.sql
+\i database/funcoes/projeto.sql
+\i database/funcoes/membro_do_projeto.sql
+\i database/funcoes/fase.sql
+\i database/funcoes/atividade.sql
+\i database/funcoes/atividade_do_membro.sql
+\i database/funcoes/artefato.sql
+\i database/funcoes/artefato_atividade.sql
+\i database/funcoes/recurso.sql
+\i database/funcoes/despesa.sql
+\i database/funcoes/mensagem.sql
+\i database/funcoes/mensagem_enviada.sql
--- database/roles.sql
+++ database/roles.sql
@@ -13,6 +13,18 @@ GRANT UPDATE ON atividade_id_atividade_seq TO insert;
 GRANT UPDATE ON comentario_id_comentario_seq TO insert;
 GRANT UPDATE ON despesa_id_despesa_seq TO insert;
 GRANT UPDATE ON doacao_id_doacao_seq TO insert;
+GRANT UPDATE ON fale_conosco_id_fale_conosco_seq TO insert;
+GRANT UPDATE ON fase_id_fase_seq TO insert;
+GRANT UPDATE ON forma_de_contato_id_forma_de_contato_seq TO insert;
+GRANT UPDATE ON imagem_id_imagem_seq TO insert;
+GRANT UPDATE ON log_de_erro_id_log_de_erro_seq TO insert;
+GRANT UPDATE ON membro_do_projeto_id_membro_do_projeto_seq TO insert;
+GRANT UPDATE ON mensagem_id_mensagem_seq TO insert;
+GRANT UPDATE ON nota_id_nota_seq  TO insert;
+GRANT UPDATE ON projeto_id_projeto_seq TO insert;
+GRANT UPDATE ON recurso_id_recurso_seq TO insert;
+GRANT UPDATE ON usuario_id_usuario_seq TO insert;
+
 
 CREATE ROLE retrieve;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO retrieve;
