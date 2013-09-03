ef071f3a458d1e1582f49de43d562df3342971d0
BrunoRB <brunorb8@hotmail.com>
2013-09-02 22:06:38 -0300

    roles

--- database/roles.sql
+++ database/roles.sql
@@ -30,18 +30,3 @@ GRANT DELETE ON ALL TABLES IN SCHEMA public TO delete;
 
 GRANT USAGE ON SCHEMA public TO insert, retrieve, update, delete, function;
 
-
-CREATE OR REPLACE FUNCTION ups() RETURNS VOID AS $$
-	BEGIN
-		SET ROLE admin;
-		GRANT SELECT ON ALL TABLES IN SCHEMA public TO update;
-		SET ROLE update;
-		UPDATE USUARIO SET nome='UPDATE' WHERE id_usuario = 1;
-		SET ROLE admin;
-		REVOKE SELECT ON ALL TABLES IN SCHEMA public FROM update;
-	END;
-$$ LANGUAGE PLPGSQL;
-
-
-
-
