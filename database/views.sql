33879d43ef1bdc2bc56b766505dd806c3c3fe258
Maikon Marin <maikon_marin@hotmail.com>
2013-09-04 18:16:40 -0300

    nova view para a listagem de todos os membros de um projeto

--- database/views.sql
+++ database/views.sql
@@ -1,27 +1,39 @@
 -- View do Cronograma
 
 CREATE OR REPLACE VIEW cronogramView AS
-SELECT
-"public".membro_do_projeto.fk_projeto AS projeto,  -- ID para identificar o projeto a qual pertencem essas atividades, utilizado para filtar as atividades por projeto
-"public".atividade.id_atividade AS codigo, 
-"public".atividade.nome_atividade AS atividade,
-"public".atividade.inicio_atividade AS data_inicio,
-"public".atividade.limite_atividade AS data_limite,
-"public".atividade.fim_atividade AS data_fim,
-"public".atividade.fk_predecessora AS predecessora,
-"public".fase.nome AS fase,
-Count("public".atividade_do_membro.fk_membro_do_projeto) AS qtd_membro, -- Conta a quantidade de membros na atividade
-Count("public".artefato_atividade.fk_artefato) AS qtd_artefato          -- Conta a quantidade de artefatos gerados pela atividade
-FROM "public".atividade
-INNER JOIN "public".atividade_do_membro ON "public".atividade_do_membro.fk_atividade = "public".atividade.id_atividade
-INNER JOIN "public".artefato_atividade ON "public".artefato_atividade.fk_atividade = "public".atividade.id_atividade
-INNER JOIN "public".membro_do_projeto ON "public".atividade_do_membro.fk_membro_do_projeto = "public".membro_do_projeto.id_membro_do_projeto
-INNER JOIN "public".fase ON "public".atividade.fk_fase = "public".fase.id_fase
-GROUP BY "public".membro_do_projeto.fk_projeto, "public".atividade.id_atividade, "public".fase.id_fase 
-ORDER BY fase.id_fase; -- Ordenando as atividade pela sua fase
+	SELECT
+	"public".membro_do_projeto.fk_projeto AS projeto,  -- ID para identificar o projeto a qual pertencem essas atividades, utilizado para filtar as atividades por projeto
+	"public".atividade.id_atividade AS codigo, 
+	"public".atividade.nome_atividade AS atividade,
+	"public".atividade.inicio_atividade AS data_inicio,
+	"public".atividade.limite_atividade AS data_limite,
+	"public".atividade.fim_atividade AS data_fim,
+	"public".atividade.fk_predecessora AS predecessora,
+	"public".fase.nome AS fase,
+	Count("public".atividade_do_membro.fk_membro_do_projeto) AS qtd_membro, -- Conta a quantidade de membros na atividade
+	Count("public".artefato_atividade.fk_artefato) AS qtd_artefato          -- Conta a quantidade de artefatos gerados pela atividade
+	FROM "public".atividade
+		INNER JOIN "public".atividade_do_membro ON "public".atividade_do_membro.fk_atividade = "public".atividade.id_atividade
+		INNER JOIN "public".artefato_atividade ON "public".artefato_atividade.fk_atividade = "public".atividade.id_atividade
+		INNER JOIN "public".membro_do_projeto ON "public".atividade_do_membro.fk_membro_do_projeto = "public".membro_do_projeto.id_membro_do_projeto
+		INNER JOIN "public".fase ON "public".atividade.fk_fase = "public".fase.id_fase
+			GROUP BY "public".membro_do_projeto.fk_projeto, "public".atividade.id_atividade, "public".fase.id_fase 
+			ORDER BY fase.id_fase; -- Ordenando as atividade pela sua fase
 
 --View Listagem de projetos
 CREATE OR REPLACE VIEW projetoView AS 
 	SELECT projeto.id_projeto, projeto.nome, membro_do_projeto.funcao, usuario.id_usuario FROM projeto 
 		INNER JOIN membro_do_projeto ON projeto.id_projeto = membro_do_projeto.fk_projeto
 		INNER JOIN usuario ON membro_do_projeto.fk_usuario = usuario.id_usuario;
+
+
+
+-- View Listagem dos membros de um projeto
+CREATE OR REPLACE VIEW membro_projetoView AS
+	SELECT membro_do_projeto.fk_projeto AS projeto, usuario.nome AS membro, membro_do_projeto.funcao, Count(atividade_do_membro.fk_atividade) AS qtd_atividade 
+	FROM membro_do_projeto
+		INNER JOIN usuario ON membro_do_projeto.fk_usuario = usuario.id_usuario
+		INNER JOIN atividade_do_membro ON atividade_do_membro.fk_membro_do_projeto = membro_do_projeto.id_membro_do_projeto
+		INNER JOIN atividade ON atividade_do_membro.fk_atividade = atividade.id_atividade
+			WHERE atividade.finalizada = FALSE
+			GROUP BY atividade_do_membro.fk_membro_do_projeto, membro_do_projeto.id_membro_do_projeto, usuario.id_usuario;
